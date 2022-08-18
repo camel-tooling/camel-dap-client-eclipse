ff2db3f4881a05fa13b7414e837ab82050b0deaf
Filip Pospisil <fpospisi@redhat.com>
2022-08-18 10:58:25 +0200

    Adding some basic stuff to work with DAP.

---
+++ com.github.camel-tooling.dap.eclipse.client.tests.ui/plugins/com.github.cameltooling.dap.reddeer/src/com/github/cameltooling/dap/reddeer/utils/CreateNewEmptyFile.java
@@ -0,0 +1,68 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cameltooling.dap.reddeer.utils;
+
+import com.github.cameltooling.dap.reddeer.wizard.NewGenericFileDialog;
+import com.github.cameltooling.dap.reddeer.wizard.NewGenericFileWizard;
+import com.github.cameltooling.dap.reddeer.wizard.NewJavaClassFirstPage;
+import com.github.cameltooling.dap.reddeer.wizard.NewJavaClassWizard;
+import com.github.cameltooling.dap.reddeer.wizard.NewXMLFileFirstPage;
+import com.github.cameltooling.dap.reddeer.wizard.NewXMLFileWizard;
+
+/**
+ * Creates new empty file.
+ *
+ * @author fpospisi
+ */
+public class CreateNewEmptyFile {
+
+	public static void genericFile(String filename) {
+		NewGenericFileWizard newGenFile = new NewGenericFileWizard();
+		newGenFile.open();
+		NewGenericFileDialog newGenFileDialog = new NewGenericFileDialog(newGenFile);
+		newGenFileDialog.setFileName(filename);
+		newGenFile.finish();
+	}
+
+	/**
+	 * Creates new empty XML file.
+	 *
+	 * @param filename Name of file.
+	 */
+	public static void XMLFile(String filename) {
+		NewXMLFileWizard newWizXML = new NewXMLFileWizard();
+		newWizXML.open();
+		NewXMLFileFirstPage newWizXMLPage = new NewXMLFileFirstPage(newWizXML);
+		newWizXMLPage.setFileName(filename);
+		newWizXML.finish();
+	}
+
+	/**
+	 * Creates new empty Java class.
+	 *
+	 * @param filename Name of class.
+	 */
+	public static void JavaClass(String filename) {
+		NewJavaClassWizard newWizJavaClass = new NewJavaClassWizard();
+
+		newWizJavaClass.open();
+		NewJavaClassFirstPage newWizJavaClassPage = new NewJavaClassFirstPage(newWizJavaClass);
+		newWizJavaClassPage.setClassName(filename);
+
+		newWizJavaClass.finish();
+	}
+}
---
+++ com.github.camel-tooling.dap.eclipse.client.tests.ui/plugins/com.github.cameltooling.dap.reddeer/src/com/github/cameltooling/dap/reddeer/utils/JavaProjectFactory.java
@@ -0,0 +1,75 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cameltooling.dap.reddeer.utils;
+
+import org.eclipse.reddeer.common.wait.AbstractWait;
+import org.eclipse.reddeer.common.wait.TimePeriod;
+import org.eclipse.reddeer.common.wait.WaitWhile;
+import org.eclipse.reddeer.eclipse.jdt.ui.wizards.JavaProjectWizard;
+import org.eclipse.reddeer.eclipse.ui.navigator.resources.ProjectExplorer;
+import org.eclipse.reddeer.eclipse.utils.DeleteUtils;
+import org.eclipse.reddeer.eclipse.wst.jsdt.ui.wizards.JavaProjectWizardFirstPage;
+import org.eclipse.reddeer.swt.api.Shell;
+import org.eclipse.reddeer.swt.impl.button.CheckBox;
+import org.eclipse.reddeer.swt.impl.button.PushButton;
+import org.eclipse.reddeer.swt.impl.menu.ContextMenuItem;
+import org.eclipse.reddeer.swt.impl.shell.DefaultShell;
+import org.eclipse.reddeer.workbench.core.condition.JobIsRunning;
+import org.eclipse.reddeer.workbench.impl.shell.WorkbenchShell;
+
+/**
+ * Creates new Java project.
+ *
+ * @author fpospisi
+ */
+public class JavaProjectFactory {
+
+	/**
+	 * Creates new Java project.
+	 *
+	 * @param name Name of project.
+	 */
+	public static void create(String name) {
+		JavaProjectWizard javaWiz = new JavaProjectWizard();
+		javaWiz.open();
+		JavaProjectWizardFirstPage javaWizPage = new JavaProjectWizardFirstPage(javaWiz);
+		javaWizPage.setName(name);
+		javaWiz.finish(TimePeriod.DEFAULT);
+	}
+
+	/**
+	 * Removes all projects.
+	 */
+	public static void deleteAllProjects() {
+		ProjectExplorer explorer = new ProjectExplorer();
+		explorer.activate();
+		if (explorer.getProjects().size() > 0) {
+			explorer.selectAllProjects();
+			new ContextMenuItem("Refresh").select();
+			new WaitWhile(new JobIsRunning(), TimePeriod.LONG);
+			AbstractWait.sleep(TimePeriod.SHORT);
+			new WorkbenchShell();
+			explorer.activate();
+			explorer.selectAllProjects();
+			new ContextMenuItem("Delete").select();
+			Shell s = new DefaultShell("Delete Resources");
+			new CheckBox().toggle(true);
+			new PushButton("OK").click();
+			DeleteUtils.handleDeletion(s, TimePeriod.LONG);
+		}
+	}
+}
